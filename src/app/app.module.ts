231b9784b56d707b13bee9450560b04b37ad60cc
Eduardo Gonzalez <jose_e_gonzalez@homedepot.com>
2019-04-10 15:18:44 -0400

    Setup angular app with MVC file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpModule } from '@angular/http';
+import { FormsModule }   from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { DataService } from './Todos/Services/data.service';
+import { AppRoutes } from './app.routes';
+
+import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatMenuModule, MatIconModule } from '@angular/material';
+import { TodoFormComponent } from './Todos/Components/todo-form/todo-form.component';
+import { TodoListComponent } from './Todos/Components/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoFormComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(AppRoutes),
+    HttpModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
